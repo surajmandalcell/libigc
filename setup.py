f323efbacc01c128b9febd1c56560600f5ba1b40
Suraj Mandal <surajmandalcell@gmail.com>
2024-09-12 05:00:40 -0400

    Bump version: 1.0.3 → 1.0.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="libigc",
-    version="1.0.3",
+    version="1.0.4",
     author="Suraj Mandal",
     author_email="dev@mandalsuraj.com",
     description="A library for parsing IGC files and extracting thermals",
